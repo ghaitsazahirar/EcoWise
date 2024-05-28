8de1bf2c7b6519777a7060d2581e15beb1740d9c
Ghaitsa Zahira Rahma <ghaitsazahira007@gmail.com>
2024-05-28 14:49:10 +0700

    menambahkan autentikasi untuk sign up

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+import { auth, database } from './firebase-config.js';
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
+import { ref, set } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
+
+document.getElementById('signup-button').addEventListener('click', function(event) {
     event.preventDefault();
+    const name = document.getElementById('nama').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Tindakan yang dilakukan saat formulir disubmit
-    console.log('Email:', email);
-    console.log('Password:', password);
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
 
-    // Misalnya, Anda bisa mengirim data ini ke server atau menggunakan Firebase Authentication
+            set(ref(database, 'users/' + user.uid), {
+                nama: name,
+                email: email,
+                password: password
+            })
+            .then(() => {
+                alert("User telah sukses dibuat");
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+        });
 });
