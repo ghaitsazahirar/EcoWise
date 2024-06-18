f23d3340d983e7564b186fbfa21fad6853a7f5af
Irvan-Yusuf <211110048@student.mercubuana-yogya.ac.id>
2024-06-18 20:08:51 +0700

    add blur

--- src/scripts/function-nav.js
+++ src/scripts/function-nav.js
@@ -2,18 +2,23 @@
 export function toggleMenu() {
     const navMenu = document.querySelector('.nav-menu');
     const headerContent = document.querySelector('.header-content');
+    const mainContent = document.querySelector('main');
     
     navMenu.classList.toggle('active');
     headerContent.classList.toggle('blur');
+    mainContent.classList.toggle('blur');
 }
 
 export function closeMenu() {
     const navMenu = document.querySelector('.nav-menu');
     const headerContent = document.querySelector('.header-content');
+    const mainContent = document.querySelector('main');
     
     navMenu.classList.remove('active');
     headerContent.classList.remove('blur');
+    mainContent.classList.remove('blur');
 }
+
 
 export function navigateToChallenge(type) {
     window.location.href = 'kategoritantangan.html?type=' + type;
